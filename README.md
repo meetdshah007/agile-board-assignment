df5dbd65c825767d29ce93b895a79b511ab3294c
Meet Shah <meetdshah007@gmail.com>
2020-05-24 12:10:30 +0530

    Bootstrap integration and component usage.

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Angular Concepts:
 Lazy Loading
 Unit Testing
 E2E Testing
+Bootstrap
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'board'
+}, {
+  path: 'board',
+  loadChildren: './pages/board/board.module#BoardModule'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
